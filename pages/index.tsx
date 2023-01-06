695c835881e5ef873d7c43d18c410244344830ca
Wyatt Walker <rwyattwalker@gmail.com>
2023-01-06 10:41:01 -0600

    Meta Data

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,6 +49,8 @@ export default function Home() {
       <Head>
         <title>Web Services | Evolve</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Evolve Web Services" />
+        <meta property="og:image" content="/evolve-white.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="h-[100vh] flex ">
